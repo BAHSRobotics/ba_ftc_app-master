309c388d1d27b77c3f186d9470e1b07aad7c590e
Caleb Trevino <calebtrevinoe@gmail.com>
2017-09-27 13:25:15 -0500

    7th pd commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OtherAutoMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OtherAutoMode.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.vuforia.Vuforia;
+
 /**
  * Created by 4014465 on 9/22/2017.
  */
 
 public class OtherAutoMode {
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAutonomous8514.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAutonomous8514.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.internal.vuforia.VuforiaLocalizerImpl;
+
 /**
  * Created by 4014465 on 9/22/2017.
  */
