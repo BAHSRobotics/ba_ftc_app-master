ffebb798bcc7d69470ba7d4f40be21cdc978a0e5
Caleb <calebtrevinoe@gmail.com>
2017-11-08 17:30:18 -0600

    Renamed MotorControl to TankDrivefor clarity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SharedFiles/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SharedFiles/TankDrive.java
@@ -7,13 +7,13 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  * Created by 3115250 on 11/8/2017.
  */
 
-public class MotorControl {
+public class TankDrive {
     private DcMotor frontLeft = null;
     private DcMotor frontRight = null;
     private DcMotor backLeft = null;
     private DcMotor backRight = null;
 
-    public MotorControl(HardwareMap hardwareMap){
+    public TankDrive(HardwareMap hardwareMap){
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
