2a2e63430e23e619e5419ce1f543d11fc029eb58
Caleb <calebtrevinoe@gmail.com>
2017-11-08 17:35:25 -0600

    Added LinearSlide

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SharedFiles/LinearSlide.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.SharedFiles;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by 4014465 on 11/8/2017.
+ */
+
+public class LinearSlide {
+    private DcMotor spool;
+
+}
