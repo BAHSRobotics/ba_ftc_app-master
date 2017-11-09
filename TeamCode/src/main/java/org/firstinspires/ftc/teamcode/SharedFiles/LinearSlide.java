77163c1ab79d0a45ea80f09df962733222ca63d3
Caleb <calebtrevinoe@gmail.com>
2017-11-08 18:15:36 -0600

    Finished Abstractions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SharedFiles/LinearSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SharedFiles/LinearSlide.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.SharedFiles;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.*;
 
 /**
  * Created by 4014465 on 11/8/2017.
@@ -8,5 +11,36 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class LinearSlide {
     private DcMotor spool;
+    //private int currentEncoderValue;
+    private int revolution = 1650;
+    private int n; //number of revolutions
+
+    public LinearSlide(HardwareMap hardwareMap) {
+        spool = hardwareMap.get(DcMotor.class, "spool");
+        spool.setMode(STOP_AND_RESET_ENCODER);
+        spool.setMode(RUN_TO_POSITION);
+
+    }
+    public void extendOnce() {
+        /*spool.setTargetPosition(currentEncoderValue + 1650); // Turn the motor 1 revolution
+        currentEncoderValue = spool.getCurrentPosition();*/
+        spool.setTargetPosition(n * revolution);
+        n++;
+        spool.setPower(0.5);
+    }
+    public void retractOnce() {
+        n--;
+        spool.setTargetPosition(n * revolution);
+        spool.setPower(0.5);
+    }
+    public void extendFull() {
+        spool.setTargetPosition(4 * revolution);
+        n= 4;
+    }
+    public void retractFull(){
+        spool.setTargetPosition(0);
+        n= 0;
+    }
+
 
 }
