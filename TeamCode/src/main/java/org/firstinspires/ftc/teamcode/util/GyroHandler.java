a39cd22d14b9bffb6d6444f2b94caf6016d8b6cf
Caleb <calebtrevinoe@gmail.com>
2017-11-28 18:17:28 -0600

    Added Gyro Handler

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/GyroHandler.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import com.qualcomm.robotcore.hardware.GyroSensor;
+
+/**
+ * Created by 4014465 on 11/28/2017.
+ */
+
+public class GyroHandler {
+    private GyroSensor gyro = null;
+    public GyroHandler() {
+
+    }
+}
