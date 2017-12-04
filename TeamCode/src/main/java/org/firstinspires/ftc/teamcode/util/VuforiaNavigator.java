8676feebdb3b570597e7d5551f60251d175777f3
Caleb <calebtrevinoe@gmail.com>
2017-12-04 14:34:49 -0600

    Condenses

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/VuforiaNavigator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/VuforiaNavigator.java
@@ -1,99 +1,75 @@
 package org.firstinspires.ftc.teamcode.util;
 
+import com.qualcomm.robotcore.util.RobotLog;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.firstinspires.ftc.robotcontroller.external.samples.ConceptVuforiaNavigation.TAG;
+
 /*
  * Created by 4014465 on 11/30/2017.
  */
 
+public class VuforiaNavigator extends VuforiaHandler {
 
-public class VuforiaNavigator extends VuforiaHandler{
-    float mmPerInch        = 25.4f;
-    float mmBotWidth       = 18 * mmPerInch;            // ... or whatever is right for your robot
-    float mmFTCFieldWidth  = (12*12 - 2) * mmPerInch;   // the FTC field is ~11'10" center-to-center of the glass panels
+    private float mmPerInch = 25.4f;
+    private float mmBotWidth = 18 * mmPerInch;            // ... or whatever is right for your robot
+    private float mmFTCFieldWidth = (12 * 12 - 2) * mmPerInch;   // the FTC field is ~11'10" center-to-center of the glass panels
+    private List<VuforiaTrackable> allTrackables = new ArrayList<>();
+    private Telemetry telemetry = null;
 
     public VuforiaNavigator() {
         VuforiaTrackables stonesAndChips = vuforia.loadTrackablesFromAsset("StonesAndChips");
         VuforiaTrackable redTarget = stonesAndChips.get(0);
         redTarget.setName("RedTarget");  // Stones
 
-        VuforiaTrackable blueTarget  = stonesAndChips.get(1);
+        VuforiaTrackable blueTarget = stonesAndChips.get(1);
         blueTarget.setName("BlueTarget");  // Chips
 
-// For convenience, gather together all the trackable objects in one easily-iterable collection
-
-        List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
         allTrackables.addAll(stonesAndChips);
 
         OpenGLMatrix redTargetLocationOnField = OpenGLMatrix
- //Then we translate the target off to the RED WALL. Our translation here
- //               is a negative translation in X.
-
-                .translation(-mmFTCFieldWidth/2, 0, 0)
-                .multiplied(Orientation.getRotationMatrix(
-// First, in the fixed (field) coordinate system, we rotate 90deg in X, then 90 in Z
-
-                        AxesReference.EXTRINSIC, AxesOrder.XZX,
-                        AngleUnit.DEGREES, 90, 90, 0));
+                .translation(-mmFTCFieldWidth / 2, 0, 0)
+                .multiplied(
+                        Orientation.getRotationMatrix(
+                            AxesReference.EXTRINSIC, AxesOrder.XZX, AngleUnit.DEGREES,
+                            90, 90, 0
+                        )
+                );
         redTarget.setLocation(redTargetLocationOnField);
         RobotLog.ii(TAG, "Red Target=%s", format(redTargetLocationOnField));
 
-        /* To place the Stones Target on the Blue Audience wall:
-        * - First we rotate it 90 around the field's X axis to flip it upright
-        * - Finally, we translate it along the Y axis towards the blue audience wall.
-        */
-
         OpenGLMatrix blueTargetLocationOnField = OpenGLMatrix
- //Then we translate the target off to the Blue Audience wall.
- //               Our translation here is a positive translation in Y.
-
-                .translation(0, mmFTCFieldWidth/2, 0)
-                .multiplied(Orientation.getRotationMatrix(
- //First, in the fixed (field) coordinate system, we rotate 90deg in X
-
-                        AxesReference.EXTRINSIC, AxesOrder.XZX,
-                        AngleUnit.DEGREES, 90, 0, 0));
+                .translation(0, mmFTCFieldWidth / 2, 0)
+                .multiplied(
+                        Orientation.getRotationMatrix(
+                            AxesReference.EXTRINSIC, AxesOrder.XZX,
+                            AngleUnit.DEGREES, 90, 0, 0
+                        )
+                );
         blueTarget.setLocation(blueTargetLocationOnField);
         RobotLog.ii(TAG, "Blue Target=%s", format(blueTargetLocationOnField));
 
-    }
-    public void loopable() {
-
-    }
-}
-/*
-         * Create a transformation matrix describing where the phone is on the robot. Here, we
-         * put the phone on the right hand side of the robot with the screen facing in (see our
-         * choice of BACK camera above) and in landscape mode. Starting from alignment between the
-         * robot's and phone's axes, this is a rotation of -90deg along the Y axis.
-         *
-         * When determining whether a rotation is positive or negative, consider yourself as looking
-         * down the (positive) axis of rotation from the positive towards the origin. Positive rotations
-         * are then CCW, and negative rotations CW. An example: consider looking down the positive Z
-         * axis towards the origin. A positive rotation about Z (ie: a rotation parallel to the the X-Y
-         * plane) is then CCW, as one would normally expect from the usual classic 2D geometry.
-  */
-
         OpenGLMatrix phoneLocationOnRobot = OpenGLMatrix
-                .translation(mmBotWidth/2,0,0)
+                .translation(mmBotWidth / 2, 0, 0)
                 .multiplied(Orientation.getRotationMatrix(
                         AxesReference.EXTRINSIC, AxesOrder.YZY,
                         AngleUnit.DEGREES, -90, 0, 0));
         RobotLog.ii(TAG, "phone=%s", format(phoneLocationOnRobot));
 
-/*
-         * Let the trackable listeners we care about know where the phone is. We know that each
-         * listener is a {@link VuforiaTrackableDefaultListener} and can so safely cast because
-         * we have not ourselves installed a listener of a different type.
- */
-
-        ((VuforiaTrackableDefaultListener)redTarget.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
-        ((VuforiaTrackableDefaultListener)blueTarget.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
-
+        ((VuforiaTrackableDefaultListener) redTarget.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
+        ((VuforiaTrackableDefaultListener) blueTarget.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
 /*
          * A brief tutorial: here's how all the math is going to work:
          *
@@ -113,53 +89,37 @@ public class VuforiaNavigator extends VuforiaHandler{
          * @see VuforiaTrackableDefaultListener#getRobotLocation()
  */
 
-
-//* Wait for the game to begin
-
-        telemetry.addData(">", "Press Play to start tracking");
-        telemetry.update();
-        waitForStart();
-
-// Start tracking the data sets we care about.
-
         stonesAndChips.activate();
 
-        while (opModeIsActive()) {
-
-            for (VuforiaTrackable trackable : allTrackables) {
-/*
-                 * getUpdatedRobotLocation() will return null if no new information is available since
-                 * the last time that call was made, or if the trackable is not currently visible.
-                 * getRobotLocation() will return null if the trackable is not currently visible.
- */
-
-                telemetry.addData(trackable.getName(), ((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible() ? "Visible" : "Not Visible");    //
-
-                OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
-                if (robotLocationTransform != null) {
-                    lastLocation = robotLocationTransform;
-                }
-            }
-
-             //* Provide feedback as to where the robot was last located (if we know).
-
-
-            if (lastLocation != null) {
-                //  RobotLog.vv(TAG, "robot=%s", format(lastLocation));
-                telemetry.addData("Pos", format(lastLocation));
-            } else {
-                telemetry.addData("Pos", "Unknown");
-            }
-            telemetry.update();
-        }
     }
 
-/*
-     * A simple utility that extracts positioning information from a transformation matrix
-     * and formats it in a form palatable to a human being.
-  */
+    public void loopable() {
+        for (VuforiaTrackable trackable : allTrackables) {
+
+            telemetry.addData(trackable.getName(),
+                    (
+                            (VuforiaTrackableDefaultListener) trackable.getListener()).isVisible() ?
+                            "Visible" :
+                            "Not Visible");
+
+            OpenGLMatrix robotLocationTransform = (
+                    (VuforiaTrackableDefaultListener) trackable.getListener()
+            ).getUpdatedRobotLocation();
+            if (robotLocationTransform != null) {
+                lastLocation = robotLocationTransform;
+            }
+        }
+
+        if (lastLocation != null) {
+            telemetry.addData("Pos", format(lastLocation));
+        } else {
+            telemetry.addData("Pos", "Unknown");
+        }
+        telemetry.update();
+    }
 
     String format(OpenGLMatrix transformationMatrix) {
         return transformationMatrix.formatAsTransform();
     }
+
 }
