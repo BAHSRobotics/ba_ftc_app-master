80deecb398d5f460a574fcb3fdc1b21503b5fb00
LCHERRYHOLMES <lcherryholmes@dallasisd.org>
2018-01-20 11:54:32 -0600

    All autonomouses theoretically complete.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/GlyphCatcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/GlyphCatcher.java
@@ -16,11 +16,11 @@ class GlyphCatcher {
         leftServo = hardwareMap.get(Servo.class,"leftServo");
         rightServo = hardwareMap.get(Servo.class, "rightServo");
     }
-    void openClaw() {
+    void closeClaw() {
         leftServo.setPosition(0.7);
         rightServo.setPosition(0.3);
     }
-    void closeClaw() {
+    void openClaw() {
         leftServo.setPosition(0.30 + (1/6.0));
         rightServo.setPosition(0.7 - (1/6.0));
     }
